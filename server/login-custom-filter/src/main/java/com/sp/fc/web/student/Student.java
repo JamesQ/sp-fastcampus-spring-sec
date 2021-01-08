88dcdeeea9a808b4efd946c4add76fab684ef861
옥탑방개발자 <jongwons.choi@gmail.com>
2021-01-08 11:09:14 +0900

    add 401-login-custom-filter. JamesQ

---
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/student/Student.java
@@ -0,0 +1,13 @@
+package com.sp.fc.web.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+}

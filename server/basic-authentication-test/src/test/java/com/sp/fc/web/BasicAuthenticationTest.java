ff7523520cd04ac88847351f30b074aa48e5ab88
JamesQ <jamesq72@gmail.com>
2021-06-03 17:37:24 +0900

    Added 5-1 BasicAuthenticationFilter

--- server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
+++ server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
@@ -1,6 +1,74 @@
 package com.sp.fc.web;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.http.HttpMethod.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BasicAuthenticationTest {
 
-    
+    @LocalServerPort
+    int port;
+
+    RestTemplate client = new RestTemplate();
+
+    private String greetingUrl(String relativeUrl) {
+        return String.format("http://localhost:%d%s", port, relativeUrl);
+    }
+
+    @DisplayName("1. 인증 실패")
+    @Test
+    void test_1() {
+//        String response = client.getForObject(greetingUrl("/greeting"), String.class);
+//        System.out.println(response);
+        HttpClientErrorException exception = assertThrows(HttpClientErrorException.class, () -> {
+            client.getForObject(greetingUrl("/greeting"), String.class);
+        });
+        assertEquals(401, exception.getRawStatusCode());
+    }
+
+    @DisplayName("2. 인증 성공")
+    @Test
+    void test_2() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(
+                "user1:1111".getBytes()
+        ));
+        HttpEntity entity = new HttpEntity(null, headers);
+        ResponseEntity<String> resp = client.exchange(greetingUrl("/greeting"), GET, entity, String.class);
+
+        System.out.println(resp.getBody());
+        assertEquals("hello world!", resp.getBody());
+    }
+
+    @DisplayName("3. 인증성공2")
+    @Test
+    void test_3() {
+
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        String resp = testClient.getForObject(greetingUrl("/greeting"), String.class);
+        assertEquals("hello world!", resp);
+    }
+
+    @DisplayName("4. POST 인증")
+    @Test
+    void test_4() {
+        // csrf error check
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> resp = testClient.postForEntity(greetingUrl("/greeting"), "JamesQ", String.class);
+        assertEquals("hello JamesQ", resp.getBody());
+    }
 }

43f858a3a4cc588e6e15bcd7bd502e3f284ddf4f
JamesQ <jamesq72@gmail.com>
2021-06-04 15:55:18 +0900

    Added 5-1 Basic Authetication

--- server/login-multi-chain/src/main/java/com/sp/fc/web/MultiChainProxyApplication.java
+++ server/login-multi-chain/src/main/java/com/sp/fc/web/MultiChainProxyApplication.java
@@ -8,6 +8,7 @@ public class MultiChainProxyApplication {
 
 
     public static void main(String[] args) {
+
         SpringApplication.run(MultiChainProxyApplication.class, args);
     }
 
--- server/login-multi-chain/src/test/java/com/sp/fc/web/MultiChainProxyTest.java
+++ server/login-multi-chain/src/test/java/com/sp/fc/web/MultiChainProxyTest.java
@@ -1,6 +1,62 @@
 package com.sp.fc.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sp.fc.web.student.Student;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Base64;
+import java.util.List;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MultiChainProxyTest {
 
+    @LocalServerPort
+    int port;
 
+    RestTemplate restTemplate = new RestTemplate();
+
+    @DisplayName("2. test")
+    @Test
+    void test_2() {
+        System.out.println("TEST");
+    }
+
+    @DisplayName("1. 학생 조사")
+    @Test
+    void test_1() throws JsonProcessingException {
+        String url = format("http://localhost:%d/api/teacher/students", port);
+
+        System.out.println("url: " + url);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Basic "+ Base64.getEncoder().encodeToString(
+                "choi:1".getBytes()
+        ));
+        HttpEntity<String> entity = new HttpEntity<>("", headers);
+        // Return serialized json data
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        System.out.println("getBody(): " + response.getBody());
+
+        // Deserializing into list object
+        List<Student> list = new ObjectMapper().readValue(response.getBody(),
+                new TypeReference<List<Student>>() {
+                });
+
+        System.out.println(list);
+        assertEquals(3, list.size());
+    }
 }
